fa01217ef1994bbbd5a7ce707492024200473640
Глинский Кирилл Евгеньевич <kirill.glinskiy@levelty.ru>
2026-07-14 12:33:40 +0300

    feat: register Finam adapter in BROKER_REGISTRY

--- webapp/src/brokers/registry.ts
+++ webapp/src/brokers/registry.ts
@@ -1,7 +1,8 @@
 import { BrokerAdapter } from "./types";
 import { tbankAdapter } from "./tbank/adapter";
+import { finamAdapter } from "./finam/adapter";
 
-export const BROKER_REGISTRY: BrokerAdapter[] = [tbankAdapter];
+export const BROKER_REGISTRY: BrokerAdapter[] = [tbankAdapter, finamAdapter];
 
 export function getBrokerAdapter(brokerId: string): BrokerAdapter | undefined {
   return BROKER_REGISTRY.find((adapter) => adapter.id === brokerId);
